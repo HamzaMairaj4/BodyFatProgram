7264e581aeb9e7ad37f7ecc3c3771e8e10f9383a
HamzaMairajuddi <24450018-HamzaMairajuddi@users.noreply.replit.com>
2023-09-06 12:41:10 +0000

    BodyFatCalculation

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# Take gender
-# Take 5 girth measurements
-# Use equation based on gender
-
-# siri_equation = (495/body_density) - 450
+print('Hello World!')
 
 
-
-gender = (input("Enter your gender (f or m):\n")).lower()
-while gender != "f" and gender != "m":
-  print('Enter a valid gender')
-  gender = (input("Enter your gender (f or m):\n")).lower()
-  
-if gender == 'f':
-  measurements = [tri_]
-  for i in range(len(meas))
-  triceps = input('Enter the measurements for your triceps: ')
